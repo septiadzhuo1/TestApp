e322d31adcac34627f91084a9f0292a1fe2e1b5c
septiadzhuo1 <57296306+septiadzhuo1@users.noreply.github.com>
2020-05-05 20:40:18 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import colors from 'vuetify/es5/util/colors'
 
 export default {
   mode: 'universal',
@@ -15,6 +16,9 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
+  },
+  vuetify: {
+    optionsPath: './vuetify.options.js'
   },
   /*
   ** Customize the progress-bar color
@@ -50,7 +54,17 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: true,
-      
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
     },
     optionsPath: './vuetify.options.js'
   },
