027214b2047cc29f271b2323128c374d000a5ecb
Septiadi <septiadi@s9i.com.sg>
2020-05-05 20:37:33 +0700

    merge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/co`lors'
 
 export default {
   mode: 'universal',
@@ -16,9 +15,6 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
-  },
-  vuetify: {
-    optionsPath: './vuetify.options.js'
   },
   /*
   ** Customize the progress-bar color
@@ -54,17 +50,7 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      
     },
     optionsPath: './vuetify.options.js'
   },
